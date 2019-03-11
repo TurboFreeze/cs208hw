17e97675e8a6d27292c360e88cd29ae7fe6ed6d2
TurboFreeze <iturbofreeze@gmail.com>
2019-03-11 00:05:47 -0400

    implemented monte carlo simulation of q3

---
+++ hw2/q3.R
@@ -0,0 +1,157 @@
+##
+## q3regression.r
+##
+## Differentially private regression
+##
+## JH 2019/03/10
+##
+
+
+
+### PART (A)
+
+# optimal upper bound found in previous part
+b.optimal <- 15
+
+# data generating process
+dgp <- function (n, lambda=10) {
+  rpois(n, lambda)
+}
+
+# sign function
+sgn <- function(x) {
+  return(ifelse(x < 0, -1, 1))
+}
+
+# clamping helper function
+clamp <- function (data, a, b) {
+  data.clamped <- data
+  data.clamped[data < a] <- a
+  data.clamped[data > b] <- b
+  data.clamped
+}
+
+# laplace random draws
+rlap <- function(mu=0, s=1, size=1) {
+  p <- runif(size) - 0.5
+  draws <- mu - s * sgn(p) * log(1 - 2 * abs(p))
+  draws
+}
+
+# differentially private mean release
+dpmean <- function (data, epsilon, a=0, b) {
+  mean.actual <- mean(clamp(data, a, b))
+  
+  # generate noise by Laplace mechanism
+  data.len <- length(data)
+  sensitivity <- (b - a) / data.len
+  laplace.shift <- sensitivity / epsilon
+  noise <- rlap(s=laplace.shift, size=1)
+  
+  # inject noise
+  mean.noisy <- mean.actual + noise
+  
+  # apply clamping
+  mean.clamped <- clamp(mean.noisy, a, b)
+}
+
+
+# Differentially private regression slope release (from provided code)
+dpslope <- function (y, x, ylower=0, yupper=b.optimal, xlower=0, xupper=b.optimal, eps1, eps2){
+  x <- clamp(x, xlower, xupper)
+  y <- clamp(y, ylower, yupper)
+  
+  n <- length(x)
+  sens.sxy <- (xupper - xlower) * (yupper - ylower)
+  sens.sxx  <- (xupper - xlower)^2
+  
+  scale.sxy <- sens.sxy / eps1
+  scale.sxx <- sens.sxx / eps2
+  
+  sensitive.value <- sum((x - mean(x)) * (y - mean(y))) / sum((x - mean(x))^2) 
+  
+  release.sxy <- sum((x - mean(x)) * (y - mean(y))) + rlap(mu=0, s=scale.sxy, size=1)
+  release.sxx <- sum((x - mean(x))^2) + rlap(mu=0, s=scale.sxx, size=1)
+  
+  postprocess.beta <- release.sxy / release.sxx
+  postprocess.beta
+}
+
+# Differentially private linear regression release of intercept and slope
+dpregression <- function (y, x, ylower=0, yupper=b.optimal, xlower=0, xupper=b.optimal, epsilon) {
+  x <- clamp(x, xlower, xupper)
+  y <- clamp(y, ylower, yupper)
+  
+  # calculate dp slope
+  dpbeta <- dpslope(y, x, ylower, yupper, xlower, xupper, epsilon / 4, epsilon / 4)
+  
+  # calculate dp intercept
+  dpmean.x <- dpmean(x, epsilon / 4, b=xupper)
+  dpmean.y <- dpmean(y, epsilon / 4, b=yupper)
+  dpalpha <- dpmean.y - dpbeta * dpmean.x
+  
+  list(dpalpha=dpalpha, dpbeta=dpbeta)
+}
+
+
+
+### PART (B)
+
+n <- 1000
+alpha <- 1
+beta <- 1
+sigma <- 1
+epsilon <- 1
+
+# mean squared residuals for quantifying utility
+msr <- function (y, x, beta.hat, alpha.hat) {
+  sum((y - beta.hat * x - alpha.hat)^2) / length(y)
+}
+
+simulate <- function(n.trials=100) {
+  msrs.dp <- vector("numeric", n.trials)
+  msrs.non <- vector("numeric", n.trials)
+  for (i in 1:n.trials) {
+    # generate data
+    x <- dgp(n, lambda=10)
+    y <- beta * x + alpha + rnorm(n, 0, sigma)
+    
+    # calculate differentially private release
+    results.dp <- dpregression(y, x, epsilon=epsilon)
+    msrs.dp[i] <- msr(y, x, results.dp$dpbeta, results.dp$dpalpha)
+    
+    # calculate non-private release
+    results.non <- lm(y ~ x)
+    msrs.non[i] <- msr(y, x, coef(results.non)[[2]], coef(results.non)[[1]])
+  }
+  data.frame(n=(1:n.trials), dp=msrs.dp, non=msrs.non)
+}
+
+msrs.data <- simulate(1000)
+
+# visualize
+library(ggplot2)
+y.window <- c(0, 10)
+
+# monte carlo scatterplot
+msrs.scatterplot <- ggplot(msrs.data, aes(x=n)) +
+  geom_point(aes(y=dp), color="red", alpha=0.7, size=0.5) +
+  geom_point(aes(y=non), color="green", alpha=0.7, size=0.5) +
+  ylim(y.window) +
+  theme_bw(); msrs.scatterplot
+
+# monte carlo traceplot
+msrs.traceplot <- ggplot(msrs.data, aes(x=n)) +
+  geom_line(aes(y=dp), color="red") +
+  geom_line(aes(y=non), color="green") +
+  ylim(y.window) +
+  theme_bw(); msrs.traceplot
+
+# distribution histograms
+#hist(msrs.data$dp[msrs.data$dp < 10]) # uncomment if no ggplot
+
+library(gridExtra)
+gridplots <- grid.arrange(msrs.scatterplot, msrs.traceplot)
+
+ggsave("msrs.jpg", gridplots)
+
